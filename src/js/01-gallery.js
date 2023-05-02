4562214eaf96d445522f792f51906e6c3eecdd06
Kamil Szymański <kamil313313@gmail.com>
2023-05-02 16:20:22 +0200

    HW8 Gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryClass = document.querySelector('.gallery');
+
+let markup = '';
+galleryItems.forEach(({ preview, original, description }) => {
+  const newImage = `<li class="gallery__item">
+  <a href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/></a>
+  </li>`;
+  markup += newImage;
+});
+console.log('markup: ', markup);
+galleryClass.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+// Change code below this line
