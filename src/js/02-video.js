de5675571742be564a3d1e4dfc9e252e6234b314
Kamil Szymański <kamil313313@gmail.com>
2023-05-02 21:41:41 +0200

    lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,16 @@
 import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
